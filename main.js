60886a72bc88fc98554413f71541ee463df75117
luigi2xz <163294380+luigi2xz@users.noreply.github.com>
2024-03-27 08:04:36 -0300

    .

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 alert('Olá, seja bem vindo!');
 
-let Nome_Usuario = prompt('Qual o seu nome?');
+let Nome_Usuario = '';
 
 let texto = document.querySelector('span');
 
 texto.textContent = Nome_Usuario;
+
+while (Nome_Usuario == ''){
+Nome_Usuario = prompt('Qual o seu nome?');
+}
