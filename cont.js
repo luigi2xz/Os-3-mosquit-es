6a5bb90bf1d8fe6044a810e53c4d39932088fd0a
luigi2xz <163294380+luigi2xz@users.noreply.github.com>
2024-04-03 08:38:39 -0300

    .

--- cont.js
+++ cont.js
@@ -3,4 +3,6 @@ const botoes = document.querySelectorAll('.objetivos')
 
 for(let i = 0; i < botoes.length; i++ ){
 console.log(i);
+botoes[i].classList.add('ativo');
+botoes[i].classList.remove('ativo');
 }
