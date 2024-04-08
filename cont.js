c287e8527bd03a7c68f7992e0fa0a9e8933f98b4
luigi2xz <163294380+luigi2xz@users.noreply.github.com>
2024-04-08 11:59:06 -0300

    .

--- cont.js
+++ cont.js
@@ -1,4 +1,5 @@
 const botoes = document.querySelectorAll('.objetivos')
+const texto = document.querySelectorAll('.aba-conteudo')
 // botoes.length
 
 for(let i = 0; i < botoes.length; i++ ){
@@ -6,8 +7,10 @@ console.log(i);
 botoes[i].onclick = function(){
     for(let j = 0; j < botoes.length; j++ ){
         botoes[j].classList.remove('ativo');
+        texto[j].classList.remove('ativa');
     }
     botoes[i].classList.add('ativo');
+    texto[i].classList.add('ativa');
 }
 
 // botoes[i].classList.remove('ativo');
